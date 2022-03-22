6db6e6812fef3badcef912e85c0ef1e6236eacc5
J Semidey <mtsemidey@gmail.com>
2022-03-22 18:29:38 -0400

    set fetch function to accept user names as parameter

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,16 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make fetch request to the url
+    fetch(apiUrl).then(function(response) {
+        console.log(response);
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
 };
 
+// call function, use user name in quotes as a parameter
+// ex: getUserRepos("blindsweatyhansolo"); will return my own!
 getUserRepos();
